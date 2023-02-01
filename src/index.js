482fc8caeea35fea3ba9fc7fffdf2969049443d8
John Daniels <adeyemijohndaniels@gmail.com>
2023-02-01 17:24:18 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/videos", videoRouter);
 
