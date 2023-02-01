0c9b372442681cbb2b994569d1951843877f239a
John Daniels <adeyemijohndaniels@gmail.com>
2023-02-01 07:34:58 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const verifyToken = require("./middleware/auth");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
