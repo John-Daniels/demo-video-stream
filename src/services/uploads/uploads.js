3fc41590657c7ad564c1d44c285ff321f783023b
John Daniels <adeyemijohndaniels@gmail.com>
2023-02-02 14:47:13 +0100

    added fileSize limit

--- src/services/uploads/uploads.js
+++ src/services/uploads/uploads.js
@@ -1,6 +1,9 @@
 const multer = require("multer");
 
 const videoUpload = multer({
+  limits: {
+    fileSize: 10000000000000,
+  },
   fileFilter: (req, file, cb) => {
     if (file.mimetype === "video/mp4") {
       cb(null, true);
